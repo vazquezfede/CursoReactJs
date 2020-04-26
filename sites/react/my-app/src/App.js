ea88d14d242ffdf72d6749f59a0d22b5631bd4e8
Federico Vazquez <fvazquez@buxis.com>
2020-04-26 14:57:14 -0300

    u1-entrega-obligatoria

--- sites/react/my-app/src/App.js
+++ sites/react/my-app/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Login from './Login'
-import Registro from './Registro'
-
+import Registro from './Registro';
 
 function App() {
-  let titulo = "React app"
-  let nombre = "Test"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,11 +17,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dsaadasd
-          <Login title={titulo} />
-          <Registro title={titulo} nombre={nombre} />
-        </a>
+          Learn React
+        </a> */}
+        
+      <Registro/>
       </header>
+      
     </div>
   );
 }
--- sites/react/my-app/src/Registro.js
+++ sites/react/my-app/src/Registro.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-function Registro(props){
-
-    return(
+function Registro(){
+    return (
         <div>
-            {props.title}
-            {props.nombre}
+            <label>
+                {'Nombre '}
+                <input type="text" name="nombre" placeholder="Ingrese Nombre"  />
+            </label>
+            <br/>
+            <label>
+                {'Apellido '}
+                <input type="text" name="apellido"  placeholder="Ingrese Apellido" />
+            </label>
+            <br/>        
+            <label>
+                {'Email '}
+                <input type="text" name="email"  placeholder="Ingrese Email" />
+            </label>
+            <br/>        
+            <label>
+                {'Telefono '}
+                <input type="number" name="telefono"/>
+            </label>
+            <br/>  
+            <label>
+                {'Password '}
+                <input type="password" name="password" />
+            </label>
+            <br/>  
+            <label>
+                {'Confirmar Password '}
+                <input type="password" name="password2" />
+            </label>
         </div>
     )
 }
